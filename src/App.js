101678176b94f16f70ea5616b1a6398719d9fb19
Will Tavarez <will@Wills-MacBook-Pro.local>
2022-09-23 21:56:51 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import {WeatherCard, coordinates} from './Components/WeatherCard/WeatherCard';
+
+
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from "react-places-autocomplete";
+
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+
+const options = [
+  { name: "miami" },
+  { name: "hollywood" }
+]
+
 
 function App() {
+  const [searchResults, setSearchResults] = useState([])
+  const [address, setAddress] = useState("");
+  const [coordinates, setCoordinates] = useState<coordinates>([0,0]);
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value)
+    const latLng = await getLatLng(results[0]) 
+    setAddress(value)
+    setCoordinates([latLng.lat, latLng.lng])
+  }
+
+
+  // useEffect(() => {
+
+  // }, [searchResults])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlacesAutocomplete
+        value={address}
+        onChange={setAddress}
+        onSelect={handleSelect}>
+
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) =>  (
+          <div className='searchbox'>
+            <input className='searchbox__input' {...getInputProps({placeholder: "Search for location"})} />
+
+            <div className='searchbox__options'>
+            {loading ? <div>... loading</div> : null}
+
+            {suggestions.map(s => {
+              const style = {
+                backgroundColor: s.active? "#ADD8E6": "#f8f8f8"
+              }
+              return <div{...getSuggestionItemProps(s, {style})}>
+                 {s.description}
+               </div>
+            })}
+            </div>
+          </div>
+        ) }
+      </PlacesAutocomplete>
+      
+      <WeatherCard coordinates={coordinates}/>
+        
     </div>
   );
 }
 
 export default App;
+
