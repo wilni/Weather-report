4e817b154b4a296ea66b72e5a8d7e6ac92d7e968
Will Tavarez <will@Wills-MacBook-Pro.local>
2022-09-25 14:19:49 -0400

    card add/delete functionality added

--- src/App.js
+++ src/App.js
@@ -7,28 +7,47 @@ import { FaSearchLocation } from "react-icons/fa";
 
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 
-const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
-
-const options = [
-  { name: "miami" },
-  { name: "hollywood" }
-]
-
 
 function App() {
   const [address, setAddress] = useState("");
   const [coordinates, setCoordinates] = useState([]);
 
+  const checkForLocation = (prev, location) => {
+    let inList = false; 
+
+    prev.forEach(el => {
+      if((el.lat === location.lat) && (el.lng === location.lng)){
+        inList = true;
+      }
+    })
+
+    return inList;
+  }
+
   const handleSelect = async (value) => {
     const results = await geocodeByAddress(value)
     const latLng = await getLatLng(results[0]) 
+    let location = {lat: latLng.lat, lng:latLng.lng}
     setAddress(value)
-    setCoordinates([latLng.lat, latLng.lng])
+    setCoordinates(prev => {
+      let locationInList = checkForLocation(prev, location)
+      if(locationInList){
+        return prev.filter(loc => (loc.lat !== location.lat) && (loc.lng !== location.lng));
+      }else {
+        return [...prev, location]
+      }
+    })
+  }
+
+  const handleDelete = (location) => {
+    setCoordinates(prev => {
+        return prev.filter(loc => (loc.lat !== location.lat) && (loc.lng !== location.lng));
+    })
   }
 
 
   return (
-    <div className="App">
+    <div className={`App ${coordinates[0] ? "" : "App--empty"}`}>
       <PlacesAutocomplete
         value={address}
         onChange={setAddress}
@@ -37,10 +56,10 @@ function App() {
         {({ getInputProps, suggestions, getSuggestionItemProps, loading }) =>  (
           <div className='searchbox'>
             <FaSearchLocation className='searchbox__icon'/>
-            <input className='searchbox__input' {...getInputProps({placeholder: "Search for location"})} />
+            <input className='searchbox__input' {...getInputProps({placeholder: "Search locations"})} />
 
             <div className='searchbox__options'>
-            {loading ? <div>... loading</div> : null}
+            {loading ? <div className='loading'><p className='loading__text'>... loading</p></div> : null}
 
             {suggestions.map(s => {
               const style = {
@@ -55,7 +74,11 @@ function App() {
         ) }
       </PlacesAutocomplete>
       
-      <WeatherCard coordinates={coordinates}/>
+      <div className='card-gallery'>
+            {coordinates.map((coor, idx) => {
+              return <WeatherCard key={idx} coordinates={coor} handleDelete={handleDelete}/>
+            })}
+      </div>
         
     </div>
   );
