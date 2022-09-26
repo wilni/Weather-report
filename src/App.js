3433e1a36bc590aa6622c01cc1d15d5a2a77bd5d
Will Tavarez <will@Wills-MacBook-Pro.local>
2022-09-26 14:27:57 -0400

    added coordinates to local storage for returning users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
-import {WeatherCard} from './Components/WeatherCard/WeatherCard';
+import { WeatherCard } from './Components/WeatherCard/WeatherCard';
 import { FaSearchLocation } from "react-icons/fa";
 
 
@@ -13,10 +13,10 @@ function App() {
   const [coordinates, setCoordinates] = useState([]);
 
   const checkForLocation = (prev, location) => {
-    let inList = false; 
+    let inList = false;
 
     prev.forEach(el => {
-      if((el.lat === location.lat) && (el.lng === location.lng)){
+      if ((el.lat === location.lat) && (el.lng === location.lng)) {
         inList = true;
       }
     })
@@ -26,14 +26,15 @@ function App() {
 
   const handleSelect = async (value) => {
     const results = await geocodeByAddress(value)
-    const latLng = await getLatLng(results[0]) 
-    let location = {lat: latLng.lat, lng:latLng.lng}
+    const latLng = await getLatLng(results[0])
+    let location = { lat: latLng.lat, lng: latLng.lng }
     setAddress(value)
     setCoordinates(prev => {
-      let locationInList = checkForLocation(prev, location)
-      if(locationInList){
-        return prev.filter(loc => (loc.lat !== location.lat) && (loc.lng !== location.lng));
-      }else {
+      const locationInList = checkForLocation(prev, location)
+      if (locationInList) {
+        return handleDelete(location)
+      } else {
+        localStorage.setItem("coordinates", JSON.stringify([...prev, location]))
         return [...prev, location]
       }
     })
@@ -41,10 +42,17 @@ function App() {
 
   const handleDelete = (location) => {
     setCoordinates(prev => {
-        return prev.filter(loc => (loc.lat !== location.lat) && (loc.lng !== location.lng));
+      const newCoordinates = prev.filter(loc => (loc.lat !== location.lat) && (loc.lng !== location.lng))
+      localStorage.setItem("coordinates", JSON.stringify(newCoordinates))
+      return newCoordinates
     })
   }
 
+  
+  useEffect(() => {
+    const savedCoordinates = JSON.parse(localStorage.getItem("coordinates"))
+    setCoordinates(savedCoordinates)
+  }, [])
 
   return (
     <div className={`App ${coordinates[0] ? "" : "App--empty"}`}>
@@ -53,33 +61,33 @@ function App() {
         onChange={setAddress}
         onSelect={handleSelect}>
 
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) =>  (
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
           <div className='searchbox'>
-            <FaSearchLocation className='searchbox__icon'/>
-            <input className='searchbox__input' {...getInputProps({placeholder: "Search locations"})} />
+            <FaSearchLocation className='searchbox__icon' />
+            <input className='searchbox__input' {...getInputProps({ placeholder: "Search locations" })} />
 
             <div className='searchbox__options'>
-            {loading ? <div className='loading'><p className='loading__text'>... loading</p></div> : null}
+              {loading ? <div className='loading'><p className='loading__text'>... loading</p></div> : null}
 
-            {suggestions.map(s => {
-              const style = {
-                backgroundColor: s.active? "#6c757d":  "rgba(92, 92, 92, 0.506)"
-              }
-              return <div className='searchbox__option' key={s.placeId} {...getSuggestionItemProps(s, {style})}>
-                 {s.description}
-               </div>
-            })}
+              {suggestions.map(s => {
+                const style = {
+                  backgroundColor: s.active ? "#6c757d" : "rgba(92, 92, 92, 0.506)"
+                }
+                return <div className='searchbox__option' key={s.placeId} {...getSuggestionItemProps(s, { style })}>
+                  {s.description}
+                </div>
+              })}
             </div>
           </div>
-        ) }
+        )}
       </PlacesAutocomplete>
-      
+
       <div className='card-gallery'>
-            {coordinates.map((coor, idx) => {
-              return <WeatherCard key={idx} coordinates={coor} handleDelete={handleDelete}/>
-            })}
+        {coordinates.map((coor, idx) => {
+          return <WeatherCard key={idx} coordinates={coor} handleDelete={handleDelete} />
+        })}
       </div>
-        
+
     </div>
   );
 }
